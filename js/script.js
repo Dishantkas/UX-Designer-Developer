6935f2b05279a83a1db839b2f235dec5a42015ab
Dishantkas <dishantdhiman2014@gmail.com>
2024-09-03 04:52:00 +0530

    29

--- js/script.js
+++ js/script.js
@@ -147,3 +147,23 @@ particlesJS('particles-js',
   }
 
 );
+
+
+const navLinksEls = document.querySelectorAll('.nav_link');
+const sectionEls = document.querySelectorAll('.section');
+
+
+let =currentSection = 'home';
+window.addEventListener('scroll', () => {
+    sectionEls.forEach(sectionEl =>{
+        if(window.scrollY >= sectionEl.offsetTop - 550){
+            currentSection = sectionEl.id;
+        }
+    });
+    navLinksEls.forEach(navLinksEl=>{
+    if(navLinksEl.href.includes(currentSection)){
+        document.querySelector('.active').classList.remove('active');
+     navLinksEl.classList.add('active');
+   }
+    });
+} );
